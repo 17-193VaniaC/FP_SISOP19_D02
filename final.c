4aeb82b19218e55cdc2bff4f52541ffac31937e6
17-193VaniaC <42793858+17-193VaniaC@users.noreply.github.com>
2019-05-12 23:29:50 +0700

    masukin fungsi buat play tapi belum betul

--- final.c
+++ final.c
@@ -10,7 +10,7 @@
 
 #define BITS 8
 void *fuseit( void *ptr );
-void *playit( void *ptr );
+void *playit( void *ptr );//fungsi untuk thread ke 2 buat play dll
 
 int main()
 {
@@ -24,15 +24,70 @@ int main()
         fprintf(stderr,"Error - pthread_create() return code: %d\n",iret1);
         exit(EXIT_FAILURE);
     }
+    
+    /*
+    iret2 = pthread_create( &thread1, NULL, fuseit, (void*) simpan2); 
+    if(iret2)
+    {
+        fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
+        exit(EXIT_FAILURE);
+    }
+    */
 
     pthread_join( thread1, NULL);
+//    pthread_join( thread2, NULL);
     exit(EXIT_SUCCESS);
 
 }
 
-void *fuseit( void *ptr )
-{
+void *fuseit( void *ptr ){
     char *fusenya;
     fusenya= (char *) ptr;
     system(fusenya);
 }
+/*
+void *playit( void *ptr){
+    char message1[1000];
+    char *playyey="play";
+    while(scanf("%s",message1)){
+    if(strncmp(message,playyey,5)==0){
+    
+    mpg123_handle *mh;
+    unsigned char *buffer;
+    size_t buffer_size;
+    size_t done;
+    int err;
+
+    int driver;
+    ao_device *dev;
+
+    ao_sample_format format;
+    int channels, encoding;
+    long rate;
+    ao_initialize();
+    driver = ao_default_driver_id();
+    mpg123_init();
+    mh = mpg123_new(NULL, &err);
+    buffer_size = mpg123_outblock(mh);
+    buffer = (unsigned char*) malloc(buffer_size * sizeof(unsigned char));
+
+    mpg123_open(mh, message1[1]);
+    mpg123_getformat(mh, &rate, &channels, &encoding);
+
+    format.bits = mpg123_encsize(encoding) * BITS;
+    format.rate = rate;
+    format.channels = channels;
+    format.byte_format = AO_FMT_NATIVE;
+    format.matrix = 0;
+    dev = ao_open_live(driver, &format, NULL);
+
+    while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK)
+        ao_play(dev, buffer, done);
+
+    free(buffer);
+    ao_close(dev);
+    mpg123_close(mh);
+    mpg123_delete(mh);
+    mpg123_exit();
+    ao_shutdown();
+}*/
